d9becf65c8f0a72b4a0b136ddb8e77c2367f4492
Prince Sharma <Prince-919>
2024-08-14 21:55:16 +0530

    create pages && setup routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import SignIn from "./pages/SingIn";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-600">Hello</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
