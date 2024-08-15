58c68b91459838b95299a6f26a730de64238820b
Prince Sharma <Prince-919>
2024-08-16 01:14:07 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import dbConnect from "./config/db.js";
 import { config } from "./config/config.js";
 import userRoute from "./user/userRoute.js";
+import authRoute from "./auth/authRoute.js";
 
 const app = express();
 
+// Middlewares
+app.use(express.json());
 // Routes
 app.use("/api/user", userRoute);
+app.use("/api/auth", authRoute);
 
 const serverStart = async () => {
   await dbConnect();
