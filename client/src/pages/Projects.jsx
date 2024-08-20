79b0cbbb8bf90e553a2aaf09c2c1b33ddf3f6d55
Prince Sharma <Prince-919>
2024-08-20 22:12:44 +0530

    about and projects page completed

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
+import CallToaction from "../components/CallToAction";
+
 const Projects = () => {
   return (
-    <div>
-      <h1>Projects Page</h1>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Pojects</h1>
+      <p className="text-md text-gray-500">
+        Build fun and engaging projects while learning HTML, CSS, and
+        JavaScript!
+      </p>
+      <CallToaction />
     </div>
   );
 };
